56edf069e2361e72273c8711372d504b26f01aab
Berzeg07 <berezg86@gmail.com>
2017-06-09 11:23:55 +0400

    31.05

---
+++ index.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<title>Главная</title>
+
+	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="libs/owl.carousel/assets/owl.carousel.css" />
+
+	
+</head>
+<body>
+
+	<div class="container">
+		<div id="slider">
+
+			<div class="item">
+				<div class="item__box">
+					<img src="img/sl3.jpg" alt="">
+				</div>
+			</div>	
+
+			<div class="item">
+				<div class="item__box">
+					<img src="img/sl3.jpg" alt="">
+				</div>
+			</div>	
+
+			<div class="item">
+				<div class="item__box">
+					<img src="img/sl3.jpg" alt="">
+				</div>
+			</div>	
+
+			<div class="item">
+				<div class="item__box">
+					<img src="img/sl3.jpg" alt="">
+				</div>
+			</div>	
+
+			<div class="item">
+				<div class="item__box">
+					<img src="img/sl3.jpg" alt="">
+				</div>
+			</div>	
+
+		</div>
+	</div>
+
+
+
+
+
+
+
+	<!-- JS_BLOCK -->
+	<script src="libs/jquery/jquery-1.11.1.min.js"></script>
+	<script src="libs/owl.carousel/owl.carousel.js"></script>
+	<script src="js/common.js"></script>
+
+
+</body>
+</html>
